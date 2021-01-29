dda44582b0c30f6f8abf9ad9bf07ac46bb39e576
Topsy-Goo <topsygoo@yandex.ru>
2021-01-29 20:27:21 +0300

    ДЗ-6 Продвинутое ООП

---
+++ src/main/java/ru/geekbrains/AntonovDV/Lesson6/Animals/Dog.java
@@ -0,0 +1,43 @@
+package ru.geekbrains.AntonovDV.Lesson6.Animals;
+
+// Типы доступа расставляем из расчёта, что все классы-наследники будут располагаться внутри одного пакета.
+public class Dog extends Animal
+{
+	static int dogsCounter = 0;
+
+	public Dog (double age, String name)
+	{
+		super (false, age, name);
+		maxRunDistance = 500;
+		maxSwimDistance = 10;
+		dogsCounter ++;
+	}
+
+	public static int getDogsCounter() {   return dogsCounter;  }
+
+	@Override public void run (int distance)
+	{
+		if (distance < 0)	distance = 0;
+
+		if (distance > maxRunDistance)
+			distance = maxRunDistance;
+		System.out.printf ("%s пробежал(а) %d метров%s.\n", name, distance, ((maxRunDistance > 0) ? "" : " (т.к. не умеет бегать)"));
+	}
+
+	@Override public void swim (int distance)
+	{
+		if (distance < 0)	distance = 0;
+
+		if (distance > maxSwimDistance)
+			distance = maxSwimDistance;
+		System.out.printf ("%s проплыл(а) %d метров%s.\n", name, distance, ((maxSwimDistance > 0) ? "" : " (т.к. не умеет плавать)"));
+	}
+
+	@Override public Dog animalGone ()
+	{
+		super.animalGone();
+		dogsCounter --;
+		return null;
+	}
+
+}
