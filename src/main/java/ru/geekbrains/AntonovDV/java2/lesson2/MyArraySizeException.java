faec731bfa249149ba025baafa157d74c1ed3819
Topsy-Goo <topsygoo@yandex.ru>
2021-02-16 17:26:23 +0300

    Java2 / Урок 2 / ДЗ

---
+++ src/main/java/ru/geekbrains/AntonovDV/java2/lesson2/MyArraySizeException.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.AntonovDV.java2.lesson2;
+
+public class MyArraySizeException extends IllegalArgumentException
+{
+
+    public MyArraySizeException (String errMsg)
+    {
+        super (errMsg);
+    }
+
+
+}// class MyArraySizeException
